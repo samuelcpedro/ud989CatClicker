2ee4818a369994994558d1887ef3019287200f2c
samuelcpedro <samuelcpedro@gmail.com>
2017-04-05 02:15:10 +0000

    Index page with two cats and counting the clicks separated

--- js/helper.js
+++ js/helper.js
@@ -2,5 +2,10 @@ $(document)
 .on('click', '#img_gato', function(event) {
 	event.preventDefault();
 	/* Act on the event */
-	$('#badge_id').html(function(i,value) { return +value+1;})
+	$('#badge1_id').html(function(i,value) { return +value+1;})
+})
+.on('click', '#img_gato2', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	$('#badge2_id').html(function(i,value) { return +value+1;})
 });
